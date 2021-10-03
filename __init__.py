27a92718d6a6df3eea183695a810ec6f610de61a
Chris J. Karr <chris@audacious-software.com>
2021-10-03 13:19:57 +0000

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'simple_messaging_azure.apps.SimpleMessagingAzureConfig'
---
+++ apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class SimpleMessagingAzureConfig(AppConfig):
+    name = 'simple_messaging_azure'
+    default_auto_field = 'django.db.models.BigAutoField'

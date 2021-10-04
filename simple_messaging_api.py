64dc1bc0b4efebc7a107581499b1cc48bcddc7a4
Chris J. Karr <chris@audacious-software.com>
2021-10-04 18:52:52 +0000

    Azure SMS implementation wrap-up

--- simple_messaging_api.py
+++ simple_messaging_api.py
@@ -1,147 +1,151 @@
 # pylint: disable=line-too-long, no-member
 
-# import importlib
-# import json
-# import mimetypes
-# import time
-#
-# from io import BytesIO
-#
-# import requests
-#
-# from django.conf import settings
-# from django.core import files
-from django.http import HttpResponse
-# from django.utils import timezone
-#
-# from simple_messaging.models import IncomingMessage, IncomingMessageMedia
+from __future__ import print_function
 
-def process_outgoing_message(outgoing_message): # pylint: disable=unused-argument
-	# Implement Azure code for sending message
+import importlib
+import json
 
-    #metadata = {}
+import requests
 
-    #if hasattr(settings, 'SIMPLE_MESSAGING_TWILIO_CLIENT_ID') and hasattr(settings, 'SIMPLE_MESSAGING_TWILIO_AUTH_TOKEN'):
-    #    client = Client(settings.SIMPLE_MESSAGING_TWILIO_CLIENT_ID, settings.SIMPLE_MESSAGING_TWILIO_AUTH_TOKEN)
+from azure.communication.sms import SmsClient
 
-    #    transmission_metadata = {}
+from django.conf import settings
+from django.http import HttpResponse, Http404
+from django.utils import timezone
 
-    #    if outgoing_message.transmission_metadata is not None:
-    #        transmission_metadata = json.loads(outgoing_message.transmission_metadata)
+from simple_messaging.models import IncomingMessage, OutgoingMessage, encrypt_value
 
-    #    twilio_message = None
+def process_outgoing_message(outgoing_message):
+    metadata = {}
 
-    #    if outgoing_message.message.startswith('image:'):
-    #        twilio_message = client.messages.create(to=outgoing_message.current_destination(), from_=settings.SIMPLE_MESSAGING_TWILIO_PHONE_NUMBER, media_url=[outgoing_message.message[6:]])
+    if hasattr(settings, 'AZURE_MESSAGING_CONNECTION_STRING') and hasattr(settings, 'AZURE_MESSAGING_PHONE_NUMBER')  and hasattr(settings, 'AZURE_MESSAGING_ENABLE_DELIVERY_REPORT'):
+        sms_client = SmsClient.from_connection_string(settings.AZURE_MESSAGING_CONNECTION_STRING)
 
-    #        time.sleep(10)
-    #    else:
-    #        twilio_message = client.messages.create(to=outgoing_message.current_destination(), from_=settings.SIMPLE_MESSAGING_TWILIO_PHONE_NUMBER, body=outgoing_message.fetch_message(transmission_metadata))
+        transmission_metadata = {}
 
-    #    metadata['twilio_sid'] = twilio_message.sid
+        if outgoing_message.transmission_metadata is not None:
+            transmission_metadata = json.loads(outgoing_message.transmission_metadata)
 
-    #    return metadata
+        if outgoing_message.message.startswith('image:'):
+            pass
+        else:
+            sms_responses = sms_client.send(from_=settings.AZURE_MESSAGING_PHONE_NUMBER, to=outgoing_message.current_destination(), message=outgoing_message.fetch_message(transmission_metadata), enable_delivery_report=settings.AZURE_MESSAGING_ENABLE_DELIVERY_REPORT)
+
+            for result in sms_responses:
+                metadata['azure_message_id'] = result.message_id
+                metadata['azure_successful'] = result.successful
+                metadata['azure_error_message'] = result.error_message
+
+        return metadata
 
     return None
 
-def process_incoming_request(request): # pylint: disable=unused-argument
-    response = '<?xml version="1.0" encoding="UTF-8" ?><Response>'
+def process_incoming_request(request): # pylint: disable=too-many-locals, too-many-branches, too-many-statements
+    if request.method == 'POST': # pylint: disable=too-many-nested-blocks
+        events = json.loads(request.body)
 
-    # responses = []
+        response_payload = {}
 
-    # for app in settings.INSTALLED_APPS:
-    #     try:
-    #         response_module = importlib.import_module('.simple_messaging_api', package=app)
+        for event in events:
+            if 'validationCode' in event['data']:
+                response_payload['validationResponse'] = event['data']['validationCode']
 
-    #         responses.extend(response_module.simple_messaging_response(request.POST))
-    #     except ImportError:
-    #         pass
-    #     except AttributeError:
-    #         pass
+            if 'validationUrl' in event['data']:
+                requests.get(event['data']['validationUrl'])
 
-    # for message in responses:
-    #     response += '<Message>' + message + '</Message>'
+            if event['eventType'] == 'Microsoft.Communication.SMSReceived':
+                responses = []
 
-    # response += '</Response>'
+                for app in settings.INSTALLED_APPS:
+                    try:
+                        response_module = importlib.import_module('.simple_messaging_api', package=app)
 
-    # if request.method == 'POST': # pylint: disable=too-many-nested-blocks
-    #     record_responses = True
+                        responses.extend(response_module.simple_messaging_response(event))
+                    except ImportError:
+                        pass
+                    except AttributeError:
+                        pass
 
-    #     for app in settings.INSTALLED_APPS:
-    #         try:
-    #             response_module = importlib.import_module('.simple_messaging_api', package=app)
+                record_responses = True
 
-    #             record_responses = response_module.simple_messaging_record_response(request.POST)
-    #         except ImportError:
-    #             pass
-    #         except AttributeError:
-    #             pass
+                for app in settings.INSTALLED_APPS:
+                    try:
+                        response_module = importlib.import_module('.simple_messaging_api', package=app)
 
-    #     if record_responses:
-    #         now = timezone.now()
+                        record_responses = response_module.simple_messaging_record_response(event)
+                    except ImportError:
+                        pass
+                    except AttributeError:
+                        pass
 
-    #         destination = request.POST['To']
-    #         sender = request.POST['From']
 
-    #         incoming = IncomingMessage(recipient=destination, sender=sender)
-    #         incoming.receive_date = now
-    #         incoming.message = request.POST['Body'].strip()
-    #         incoming.transmission_metadata = json.dumps(dict(request.POST), indent=2)
+                if record_responses:
+                    now = timezone.now()
 
-    #         incoming.save()
+                    destination = event['data']['to']
+                    sender = event['data']['from']
 
-    #         incoming.encrypt_sender()
+                    incoming = IncomingMessage(recipient=destination, sender=sender)
+                    incoming.receive_date = now
+                    incoming.message = event['data']['message']
 
-    #         num_media = 0
+                    if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
+                        event['subject'] = encrypt_value(event['subject'])
+                        event['data']['to'] = encrypt_value(event['data']['to'])
+                        event['data']['from'] = encrypt_value(event['data']['from'])
 
-    #         media_objects = {}
+                    incoming.transmission_metadata = json.dumps(event, indent=2)
 
-    #         if 'NumMedia' in request.POST:
-    #             num_media = int(request.POST['NumMedia'])
+                    incoming.save()
 
-    #             for i in range(0, num_media):
-    #                 media = IncomingMessageMedia(message=incoming)
+                    incoming.encrypt_sender()
 
-    #                 media.content_url = request.POST['MediaUrl' + str(i)]
-    #                 media.content_type = request.POST['MediaContentType' + str(i)]
-    #                 media.index = i
+                    # Note - no MMS support at the moment (2021-10-04)
 
-    #                 media.save()
+                    for app in settings.INSTALLED_APPS:
+                        try:
+                            response_module = importlib.import_module('.simple_messaging_api', package=app)
 
-    #                 media_response = requests.get(media.content_url)
+                            response_module.process_incoming_message(incoming)
+                        except ImportError:
+                            pass
+                        except AttributeError:
+                            pass
 
-    #                 if media_response.status_code != requests.codes.ok:
-    #                     continue
+            elif event['eventType'] == 'Microsoft.Communication.SMSDeliveryReportReceived':
+                message_id = event['data']['messageId']
+                data = event['data']
 
-    #                 filename = media.content_url.split('/')[-1]
+                match = OutgoingMessage.objects.filter(transmission_metadata__icontains=message_id).first()
 
-    #                 extension = mimetypes.guess_extension(media.content_type)
+                if match is not None:
+                    metadata = json.loads(match.transmission_metadata)
 
-    #                 if extension is not None:
-    #                     if extension == '.jpe':
-    #                         extension = '.jpg'
+                    if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
+                        data['to'] = encrypt_value(data['to'])
+                        data['from'] = encrypt_value(data['from'])
 
-    #                     filename += extension
+                    metadata['azure_delivery_report'] = data
 
-    #                 file_bytes = BytesIO()
-    #                 file_bytes.write(media_response.content)
+                    match.transmission_metadata = json.dumps(metadata, indent=2)
+                    match.save()
 
-    #                 media.content_file.save(filename, files.File(file_bytes))
-    #                 media.save()
+                match = IncomingMessage.objects.filter(transmission_metadata__icontains=message_id).first()
 
-    #                 media_objects[filename] = {
-    #                     'content': file_bytes.getvalue(),
-    #                     'mime-type': media.content_type
-    #                 }
+                if match is not None:
+                    metadata = json.loads(match.transmission_metadata)
 
-    #         for app in settings.INSTALLED_APPS:
-    #             try:
-    #                 response_module = importlib.import_module('.simple_messaging_api', package=app)
+                    if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
+                        data['to'] = encrypt_value(data['to'])
+                        data['from'] = encrypt_value(data['from'])
 
-    #                 response_module.process_incoming_message(incoming)
-    #             except ImportError:
-    #                 pass
-    #             except AttributeError:
-    #                 pass
+                    metadata['azure_delivery_report'] = data
 
-    return HttpResponse(response, content_type='text/xml')
+                    match.transmission_metadata = json.dumps(metadata, indent=2)
+                    match.save()
+            else:
+                print('Unknown event type: ' + event['eventType'])
+
+            return HttpResponse(json.dumps(response_payload, indent=2), content_type='application/json')
+
+    raise Http404("No method found to process incoming message.")

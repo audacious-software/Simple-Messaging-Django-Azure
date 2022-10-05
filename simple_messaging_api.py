29ca870535edfd1bfb00cb9db4e254d4434af10d
Chris J. Karr <chris@audacious-software.com>
2022-10-05 20:12:39 +0000

    Switchboard work

--- simple_messaging_api.py
+++ simple_messaging_api.py
@@ -10,16 +10,46 @@ import requests
 from azure.communication.sms import SmsClient
 
 from django.conf import settings
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.utils import timezone
 
 from simple_messaging.models import IncomingMessage, OutgoingMessage, encrypt_value
 
-def process_outgoing_message(outgoing_message):
+def process_outgoing_message(outgoing_message, metadata=None): # pylint: disable=too-many-branches
+    if metadata is None:
+        metadata = {}
+
+    azure_connection_string = metadata.get('connection_string', None)
+
+    if azure_connection_string is None:
+        if hasattr(settings, 'AZURE_MESSAGING_CONNECTION_STRING'):
+            azure_connection_string = settings.AZURE_MESSAGING_CONNECTION_STRING
+        else:
+            return None
+    else:
+        del metadata['connection_string']
+
+    azure_phone_number = metadata.get('phone_number', None)
+
+    if azure_phone_number is None:
+        if hasattr(settings, 'AZURE_MESSAGING_PHONE_NUMBER'):
+            azure_phone_number = settings.AZURE_MESSAGING_PHONE_NUMBER
+        else:
+            return None
+    else:
+        del metadata['phone_number']
+
+    azure_enable_delivery_report = metadata.get('enable_delivery_report', None)
+
+    if azure_enable_delivery_report is None and hasattr(settings, 'AZURE_MESSAGING_ENABLE_DELIVERY_REPORT'):
+        azure_enable_delivery_report = settings.AZURE_MESSAGING_ENABLE_DELIVERY_REPORT
+    else:
+        del metadata['enable_delivery_report']
+
     transmission_metadata = {}
 
-    if hasattr(settings, 'AZURE_MESSAGING_CONNECTION_STRING') and hasattr(settings, 'AZURE_MESSAGING_PHONE_NUMBER')  and hasattr(settings, 'AZURE_MESSAGING_ENABLE_DELIVERY_REPORT'):
-        sms_client = SmsClient.from_connection_string(settings.AZURE_MESSAGING_CONNECTION_STRING)
+    if (azure_connection_string is not None) and (azure_phone_number is not None) and (azure_enable_delivery_report is not None):
+        sms_client = SmsClient.from_connection_string(azure_connection_string)
 
         if outgoing_message.transmission_metadata is not None:
             transmission_metadata = json.loads(outgoing_message.transmission_metadata)
@@ -29,7 +59,7 @@ def process_outgoing_message(outgoing_message):
         else:
             message = outgoing_message.fetch_message(transmission_metadata)
 
-            sms_responses = sms_client.send(from_=settings.AZURE_MESSAGING_PHONE_NUMBER, to=outgoing_message.current_destination(), message=message, enable_delivery_report=settings.AZURE_MESSAGING_ENABLE_DELIVERY_REPORT)
+            sms_responses = sms_client.send(from_=azure_phone_number, to=outgoing_message.current_destination(), message=message, enable_delivery_report=azure_enable_delivery_report)
 
             for result in sms_responses:
                 transmission_metadata['azure_message_id'] = result.message_id
@@ -42,115 +72,125 @@ def process_outgoing_message(outgoing_message):
 
 def process_incoming_request(request): # pylint: disable=too-many-locals, too-many-branches, too-many-statements
     if request.method == 'POST': # pylint: disable=too-many-nested-blocks
-        events = json.loads(request.body)
+        try:
+            events = json.loads(request.body)
 
-        response_payload = {}
+            is_azure = False
 
-        for event in events:
-            if 'validationCode' in event['data']:
-                response_payload['validationResponse'] = event['data']['validationCode']
+            for event in events:
+                if event['eventType'] in ('Microsoft.Communication.SMSReceived', 'Microsoft.Communication.SMSDeliveryReportReceived',):
+                    is_azure = True
 
-            if 'validationUrl' in event['data']:
-                requests.get(event['data']['validationUrl'], timeout=120)
+            if is_azure:
+                response_payload = {}
 
-            if event['eventType'] == 'Microsoft.Communication.SMSReceived':
-                responses = []
+                for event in events:
+                    if 'validationCode' in event['data']:
+                        response_payload['validationResponse'] = event['data']['validationCode']
 
-                for app in settings.INSTALLED_APPS:
-                    try:
-                        response_module = importlib.import_module('.simple_messaging_api', package=app)
+                    if 'validationUrl' in event['data']:
+                        requests.get(event['data']['validationUrl'], timeout=120)
 
-                        responses.extend(response_module.simple_messaging_response(event))
-                    except ImportError:
-                        pass
-                    except AttributeError:
-                        pass
+                    if event['eventType'] == 'Microsoft.Communication.SMSReceived':
+                        responses = []
 
-                record_responses = True
+                        for app in settings.INSTALLED_APPS:
+                            try:
+                                response_module = importlib.import_module('.simple_messaging_api', package=app)
 
-                for app in settings.INSTALLED_APPS:
-                    try:
-                        response_module = importlib.import_module('.simple_messaging_api', package=app)
+                                responses.extend(response_module.simple_messaging_response(event))
+                            except ImportError:
+                                pass
+                            except AttributeError:
+                                pass
 
-                        record_responses = response_module.simple_messaging_record_response(event)
-                    except ImportError:
-                        pass
-                    except AttributeError:
-                        pass
+                        record_responses = True
+
+                        for app in settings.INSTALLED_APPS:
+                            try:
+                                response_module = importlib.import_module('.simple_messaging_api', package=app)
+
+                                record_responses = response_module.simple_messaging_record_response(event)
+                            except ImportError:
+                                pass
+                            except AttributeError:
+                                pass
 
 
-                if record_responses:
-                    now = timezone.now()
+                        if record_responses:
+                            now = timezone.now()
 
-                    destination = event['data']['to']
-                    sender = event['data']['from']
+                            destination = event['data']['to']
+                            sender = event['data']['from']
 
-                    incoming = IncomingMessage(recipient=destination, sender=sender)
-                    incoming.receive_date = now
-                    incoming.message = event['data']['message']
+                            incoming = IncomingMessage(recipient=destination, sender=sender)
+                            incoming.receive_date = now
+                            incoming.message = event['data']['message']
 
-                    if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
-                        event['subject'] = encrypt_value(event['subject'])
-                        event['data']['to'] = encrypt_value(event['data']['to'])
-                        event['data']['from'] = encrypt_value(event['data']['from'])
+                            if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
+                                event['subject'] = encrypt_value(event['subject'])
+                                event['data']['to'] = encrypt_value(event['data']['to'])
+                                event['data']['from'] = encrypt_value(event['data']['from'])
 
-                    incoming.transmission_metadata = json.dumps(event, indent=2)
+                            incoming.transmission_metadata = json.dumps(event, indent=2)
 
-                    incoming.save()
+                            incoming.save()
 
-                    incoming.encrypt_sender()
+                            incoming.encrypt_sender()
 
-                    # Note - no MMS support at the moment (2021-10-04)
+                            # Note - no MMS support at the moment (2021-10-04)
 
-                    for app in settings.INSTALLED_APPS:
-                        try:
-                            response_module = importlib.import_module('.simple_messaging_api', package=app)
+                            for app in settings.INSTALLED_APPS:
+                                try:
+                                    response_module = importlib.import_module('.simple_messaging_api', package=app)
 
-                            response_module.process_incoming_message(incoming)
-                        except ImportError:
-                            pass
-                        except AttributeError:
-                            pass
+                                    response_module.process_incoming_message(incoming)
+                                except ImportError:
+                                    pass
+                                except AttributeError:
+                                    pass
 
-            elif event['eventType'] == 'Microsoft.Communication.SMSDeliveryReportReceived':
-                message_id = event['data']['messageId']
-                data = event['data']
+                    elif event['eventType'] == 'Microsoft.Communication.SMSDeliveryReportReceived':
+                        message_id = event['data']['messageId']
+                        data = event['data']
 
-                match = OutgoingMessage.objects.filter(transmission_metadata__icontains=message_id).first()
+                        match = OutgoingMessage.objects.filter(transmission_metadata__icontains=message_id).first()
 
-                if match is not None:
-                    metadata = json.loads(match.transmission_metadata)
+                        if match is not None:
+                            metadata = json.loads(match.transmission_metadata)
 
-                    if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
-                        data['to'] = encrypt_value(data['to'])
-                        data['from'] = encrypt_value(data['from'])
+                            if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
+                                data['to'] = encrypt_value(data['to'])
+                                data['from'] = encrypt_value(data['from'])
 
-                    metadata['azure_delivery_report'] = data
+                            metadata['azure_delivery_report'] = data
 
-                    if metadata['azure_delivery_report'].get('deliveryStatus', 'Unknown') != 'Delivered': # pylint: disable=simplifiable-if-statement
-                        match.errored = True
+                            if metadata['azure_delivery_report'].get('deliveryStatus', 'Unknown') != 'Delivered': # pylint: disable=simplifiable-if-statement
+                                match.errored = True
+                            else:
+                                match.errored = False
+
+                            match.transmission_metadata = json.dumps(metadata, indent=2)
+                            match.save()
+
+                        match = IncomingMessage.objects.filter(transmission_metadata__icontains=message_id).first()
+
+                        if match is not None:
+                            metadata = json.loads(match.transmission_metadata)
+
+                            if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
+                                data['to'] = encrypt_value(data['to'])
+                                data['from'] = encrypt_value(data['from'])
+
+                            metadata['azure_delivery_report'] = data
+
+                            match.transmission_metadata = json.dumps(metadata, indent=2)
+                            match.save()
                     else:
-                        match.errored = False
+                        print('Unknown event type: ' + event['eventType'])
 
-                    match.transmission_metadata = json.dumps(metadata, indent=2)
-                    match.save()
+                    return HttpResponse(json.dumps(response_payload, indent=2), content_type='application/json')
+        except json.JSONDecodeError:
+            return None
 
-                match = IncomingMessage.objects.filter(transmission_metadata__icontains=message_id).first()
-
-                if match is not None:
-                    metadata = json.loads(match.transmission_metadata)
-
-                    if hasattr(settings, 'SIMPLE_MESSAGING_SECRET_KEY'):
-                        data['to'] = encrypt_value(data['to'])
-                        data['from'] = encrypt_value(data['from'])
-
-                    metadata['azure_delivery_report'] = data
-
-                    match.transmission_metadata = json.dumps(metadata, indent=2)
-                    match.save()
-            else:
-                print('Unknown event type: ' + event['eventType'])
-
-            return HttpResponse(json.dumps(response_payload, indent=2), content_type='application/json')
-
-    raise Http404("No method found to process incoming message.")
+    return None

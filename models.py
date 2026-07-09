ffbb5a5dab044f42c48ee013b76842c9d8887595
Chris J. Karr <chris@audacious-software.com>
2026-07-09 17:52:10 -0500

    CI fix for check before migrations

--- models.py
+++ models.py
@@ -8,12 +8,15 @@ def check_azure_settings_defined(app_configs, **kwargs): # pylint: disable=unuse
     errors = []
 
     if 'simple_messaging_switchboard' in settings.INSTALLED_APPS:
-        from simple_messaging_switchboard.models import Channel # pylint: disable=import-outside-toplevel, import-error
+        try:
+            from simple_messaging_switchboard.models import Channel # pylint: disable=import-outside-toplevel, import-error
 
-        count = Channel.objects.filter(channel_type__package_name='simple_messaging_azure').count()
+            count = Channel.objects.filter(channel_type__package_name='simple_messaging_azure').count()
 
-        if count == 0:
-            error = Warning('simple_messaging_azure is installed alongside simple_messaging_switchboard, but no Channels are defined.', hint='Create Channel or consider removing simple_messaging_azure from settings.INSTALLED_APPS', obj=None, id='simple_messaging_azure.E001')
-            errors.append(error)
+            if count == 0:
+                error = Warning('simple_messaging_azure is installed alongside simple_messaging_switchboard, but no Channels are defined.', hint='Create Channel or consider removing simple_messaging_azure from settings.INSTALLED_APPS', obj=None, id='simple_messaging_azure.E001')
+                errors.append(error)
+        except ProgrammingError:
+            pass # Migrations not applied
 
     return errors
